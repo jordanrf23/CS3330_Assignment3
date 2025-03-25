49a951c6ff762640b8a03db60869fe7fdd3ed2a4
jordanfrancis <jordanfrancis@jordans-mbp.attlocal.net>
2025-03-25 15:26:38 -0500

    testing again

--- src/pack/Main.java
+++ src/pack/Main.java
@@ -3,8 +3,9 @@ package pack;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("working");
+		
+		System.out.println("working");
+		
 
 	}
 
