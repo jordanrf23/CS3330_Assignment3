dcac61cc4b98f4926546bd7b65db7df345d12985
jordanfrancis <jordanfrancis@mu-602169.dhcp.missouri.edu>
2025-04-04 20:18:10 -0500

    converting the note on off to an int/string

--- src/parser/CsvParser.java
+++ src/parser/CsvParser.java
@@ -24,18 +24,24 @@ public class CsvParser {
 				}
 				String[] values = line.split(" , ");
 				if(values.length != 6) {
-					throw new InvalidMidiDataException("Invalid number of fields in CSV row.");
+					continue;
 				}
 		
-				int tick = Integer.parseInt(values[0].trim());
-				int note = Integer.parseInt(values[1].trim());
-				int velocity = Integer.parseInt(values[2].trim());
-				int channel = Integer.parseInt(values[3].trim());
-				int noteOnOff = Integer.parseInt(values[4].trim());
+				int channel = Integer.parseInt(values[0].trim());
+				String noteOnOffString = values[1].trim();
+				int startEndTick = Integer.parseInt(values[2].trim());
+				int note = Integer.parseInt(values[3].trim());
+				int velocity = Integer.parseInt(values[4].trim());
 				int instrument = Integer.parseInt(values[5].trim());
-			
+				
+				int noteOnOff = 0;
+				if("Note_on_c".equals(noteOnOffString)){
+					noteOnOff = 1;
+				}else if ("Note_off_c".equals(noteOnOffString)){
+					noteOnOff = 0;
+				}
 	
-				MidiEventData data = new MidiEventData(channel, note, tick, noteOnOff, velocity, instrument);
+				MidiEventData data = new MidiEventData(channel, note, startEndTick, noteOnOff, velocity, instrument);
 				midiEvents.add(data);
 			}
 				
